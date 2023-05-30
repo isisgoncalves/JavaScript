7b5375b0ef0346b71b5b5e596bc957e953ec64be
isisgoncalves <isisvow@gmail.com>
2023-05-30 10:46:38 -0300

    Update variaveis.js

--- variaveis.js
+++ variaveis.js
@@ -1,2 +1,23 @@
-let idade = 28
+let idade = 28 
 console.log (idade)
+
+let numero = 30
+let altura = 1.77
+let nome = "Isis"
+let estudando = true
+console.log (numero, typeof numero)
+console.log (altura, typeof altura)
+console.log (nome, typeof nome)
+console.log (estudando, typeof estudando)
+
+let outra;
+console.log (outra)
+
+let nula = null
+console.log (nula)
+
+var variavel = 30
+console.log(variavel)
+
+const constante = 100
+console.log(constante)
